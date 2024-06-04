9ff4743b66fe4e8087ddf7c768b006343fee5912
Enrico Brasil <cylodabiliro@hotmail.com>
2024-06-04 18:10:49 -0300

    feature: app mobile

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, TouchableOpacity } from 'react-native';
 
 
 export default function App() {
+  const handleOnClick = () =>{
+    alert("ola")
+  };
   return (
       <SafeAreaView style={styles.bdy}>
+        <ScrollView>
+
         <View style={styles.header}>
           <Image
           source={require('./assets/chocolate.jpg')}
@@ -12,9 +17,25 @@ export default function App() {
           realizeMode="contain"
           />
         </View>
-        <View>
-        <Text style={styles.titulo}>Sobre nós</Text>
-        </View>
+        
+          <View style={styles.boxTitulo}>
+            <Text style={styles.titulo}>Sobre nós</Text>
+          </View>
+
+        
+          <View style={styles.boxTexto}>
+            <Text style={styles.texto}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+          </View>
+
+          <TouchableOpacity style={styles.boxTitulo} onPress={handleOnClick}>
+            <Text style={styles.titulo}>Nossos Bolos</Text>
+          </TouchableOpacity>
+
+          <View style={styles.boxFooter}>
+            <Text style={styles.textoFooter}>© 2015-2024 bolodavovo</Text>
+          </View>
+        
+       </ScrollView>
       </SafeAreaView>
   );
 }
@@ -24,13 +45,54 @@ const styles = StyleSheet.create({
     backgroundColor:'#F2A7D9',
   },
   bdy: {
-    backgroundColor:'#F2A7D9',
+    // backgroundColor:'#F2A7D9',
+        
   },
-  titulo: {
+
+  tituloArea:{
+    justifyContent:'center',
+    display:'flex',
+  },
+
+  boxTitulo:{
+    marginTop:'15%',
+    marginLeft:110,
     backgroundColor:'#F2A7D9',
-    fontSize:'35px',
+    borderRadius:30,
+    width:200,
+  },
+  
+  titulo: {
+    fontSize:35,
     color:'white',
     textAlign:'center',
-    marginTop:'40px',
+    borderRadius:300,
+  },
+
+  boxTexto: {
+    backgroundColor:'#F2A7D9',
+    marginTop:30,
+    borderRadius:30,
+    width:320,
+    height:500,
+    marginLeft:50,
+  },
+
+  texto: {
+    color:'white',
+    textAlign:"justify",
+    padding:20,
+    fontSize:18
+  },
+
+  boxFooter: {
+    backgroundColor:'#F2A7D9',
+    marginTop:40,
+  },
+
+  textoFooter: {
+    fontSize:20,
+    textAlign: 'center',
+    padding:20,
   },
 });
